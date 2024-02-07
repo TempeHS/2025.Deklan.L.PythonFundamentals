5d49775815c1447da07b9133c4089d861e30fdae
Deklan Low <124547401+Dek21a1@users.noreply.github.com>
2024-02-07 12:36:12 +0000

    compeleted hello.py and indoor.py, vido lec0 1:12:

--- 0-FunctionsVariables/hello.py
+++ 0-FunctionsVariables/hello.py
@@ -1,6 +1,9 @@
-# ask user for name and return var
-name = input("what is your name? ")
+# ask user for name and return var while removing whitespace from str & capping user name
+name = input("what is your name? ").strip().title()
+
 
 # respond hello to user, add var
-print("hello, " + name)
+# print("hello, " + name)
 # can also be coded as print("hello,", name)
+# alternatively
+print(f"hello, {name}")
---
+++ 0-FunctionsVariables/indoor.py
@@ -0,0 +1,5 @@
+# what is to be said lowercased and space corrected
+said = str(input("what do you want to say at a normal voice?")).lower().strip()
+
+# say what has to be said
+print(said)
