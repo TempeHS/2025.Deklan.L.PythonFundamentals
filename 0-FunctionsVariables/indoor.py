2a58a2b178e243415d2f0d6b78d25f2385dc2621
Deklan Low <124547401+Dek21a1@users.noreply.github.com>
2024-02-07 23:35:59 +0000

    tweaked indoor.py, completed playback.py, and started faces.py

--- 0-FunctionsVariables/indoor.py
+++ 0-FunctionsVariables/indoor.py
@@ -1,5 +1,5 @@
 # what is to be said lowercased and space corrected
-said = str(input("what do you want to say at a normal voice?")).lower().strip()
+said = str(input("what do you want to say at a normal voice? ")).lower()
 
 # say what has to be said
 print(said)
--- 0-FunctionsVariables/playback.py
+++ 0-FunctionsVariables/playback.py
@@ -1,2 +1,5 @@
-slowdown = str(input("what do you want to say slow? "))
+# ask the user for input while replaceing spaces with three dots
+slowdown = str.replace(input("what do you want to say slow? "), " ", "...")
+
+# say it
 print(slowdown)
