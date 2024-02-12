0a12a04005169641956e4cb1d20757a93bfe4051
Deklan Low <124547401+Dek21a1@users.noreply.github.com>
2024-02-12 00:29:24 +0000

    completed einstein.py

--- 0-FunctionsVariables/einstein.py
+++ 0-FunctionsVariables/einstein.py
@@ -1,4 +1,7 @@
+# ask user for input and convert to int
 mass = int(input("what is mass? "))
-energy = int(x=(mass * 300000000) ^ 2)
+# insert user mass input into into the formula E = MC^2
+energy = int(mass * 300000000**2)
 
+# produce output
 print(energy, "J")
