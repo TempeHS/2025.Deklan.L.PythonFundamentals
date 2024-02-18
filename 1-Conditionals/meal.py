0771c5184170068d9279540110c839a934d8d890
Deklan Low <124547401+Dek21a1@users.noreply.github.com>
2024-02-18 10:27:31 +0000

    completed meal.py

--- 1-Conditionals/meal.py
+++ 1-Conditionals/meal.py
@@ -4,7 +4,26 @@ def main():
 
 def meal(time):
     h, m = time.split(":")
-    if h
+    min = float(m) / 60
+    hrs = int(h)
+
+    if hrs == 7:
+        print("breakfast time")
+    elif hrs == 8:
+        if min == 0:
+            print("breakfast time")
+    elif hrs == 12:
+        print("lunch time")
+    elif hrs == 13:
+        if min == 0:
+            print("lunch time")
+    elif hrs == 18:
+        print("dinner time")
+    elif hrs == 19:
+        if min == 0:
+            print("dinner time")
+    else:
+        print("nothing")
 
 
 main()
