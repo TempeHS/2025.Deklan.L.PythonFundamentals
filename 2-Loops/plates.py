6a2b89915c2768e17a60e1b8e9751b3e50522883
Deklan Low <124547401+Dek21a1@users.noreply.github.com>
2024-02-26 11:30:26 +0000

    completed plates.py

--- 2-Loops/plates.py
+++ 2-Loops/plates.py
@@ -13,7 +13,23 @@ def is_valid(s):
     if chrval > 6 or chrval < 2:
         return False
     else:
-        return True
+        posval = chrval
+        for i in s:
+            posval = posval - 1
+            alphachk = posval - 1
+            if str(i).isalpha() is False:
+                if str(i).isalnum() is False:
+                    return False
+            elif str(i).isnumeric():
+                a = list(i)
+                if str(s[alphachk]).isalpha():
+                    return False
+                elif a.index("0"):
+                    return False
+                else:
+                    return True
+            else:
+                return True
 
 
 main()
