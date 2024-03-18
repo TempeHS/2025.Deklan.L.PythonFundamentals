9dfd9e3325af9e47a36acef1b05acd8779cb1728
Deklan Low <124547401+Dek21a1@users.noreply.github.com>
2024-03-18 11:57:37 +0000

    figlet.py nearly done with first function working, second function having aneuroysm

--- 4-Libraries/figlet.py
+++ 4-Libraries/figlet.py
@@ -1,18 +1,25 @@
 from pyfiglet import Figlet
 import sys
+from random import choice
 
 
 def main():
-    if len(sys.argv) == 2:
-        print(manualfont)
+    if len(sys.argv) >= 2:
+        manualfont(input("what do you want to style? "))
+    if len(sys.argv) <= 1:
+        randomfont(input("what do you want to style? "))
 
 
-def manualfont():
-    if sys.argv[1] == "-f" or sys.argv[1] == "--font":
-        f = sys.argv[2]
-        Figlet.setFont(font=f)
-        unstyledtext = input("what do you want to style? ")
-        print(f.renderText(unstyledtext))
+def manualfont(txt):
+    if str(sys.argv[1]) == "-f":
+        f = Figlet(font=sys.argv[2])
+        print(f.renderText(txt))
+
+
+def randomfont(txt):
+    figlet = Figlet()
+    f = choice([figlet.getFonts()])
+    print(f.renderText(txt))
 
 
 main()
