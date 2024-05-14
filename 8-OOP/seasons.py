0b27ba54363ef7b94e175acce0936faf10bc6512
Deklan Low <124547401+Dek21a1@users.noreply.github.com>
2024-05-14 13:10:04 +0000

    did some of seasons.py

--- 8-OOP/seasons.py
+++ 8-OOP/seasons.py
@@ -2,15 +2,22 @@ from datetime import date
 
 
 class Minutes:
+    def __init__(self, year, month, day):
+        date(year, month, day)
 
-    def main():
-        birthday = get_birthday()
+
+def main():
+    birthday = get_birthday()
+    print(date.today())
 
 
 def get_birthday():
     birthday = input("What is your birthday?(format in YYYY-MM-DD): ")
-    year = str(birthday).split("-")
-    print(year)
+    bd = str(birthday).split("-")
+    year = int(bd[0])
+    month = int(bd[1])
+    day = int(bd[2])
+    return Minutes(year, month, day)
 
 
 if __name__ == "__main__":
