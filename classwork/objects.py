606c344d633496a04e49ba2b4955568d9e37e1f6
Deklan Low <124547401+Dek21a1@users.noreply.github.com>
2024-05-22 23:41:20 +0000

    3D objects are being creation

---
+++ classwork/objects.py
@@ -0,0 +1,22 @@
+
+class obj:
+    def cube(self, x, y, p):
+        self.p = self
+        self.xy = (x, y)
+
+class coords:
+    def position(self, x, y):
+        match obj.self
+
+
+def main():
+    coordinates()
+
+
+def coordinates():
+    obj.cube((5, 5, 5), 15, 7)
+    
+
+
+if __name__ == "__main__":
+    main()
